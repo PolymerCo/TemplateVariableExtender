a10dd75f718c9cea05d78f41b8387a392c5dce67
Oliver Mitchell <oliver@polymerco.com.au>
2022-11-26 23:09:35 +1030

    Updated processor names to be consistent

--- Processors/NamespaceProcessor.cs
+++ Processors/NamespaceProcessor.cs
@@ -2,23 +2,20 @@
 using System.Linq;
 
 namespace Keyword.Processors {
-  public class NamespaceKeywordProcessor : KeywordProcessor {
-    /** Keyword to identity the Assets directory. */
-    private const string AssetsKeyword = "Assets";
-
+  public class NamespaceProcessor : KeywordProcessor {
     /** Separator used for namespaces. */
     private const string NamespaceSeparator = ".";
       
     /** Words to exclude from the namespace. */
     private static readonly string[] NamespaceExclusions = { "scripts", "src", "editor" };
     
-    public NamespaceKeywordProcessor() : base("NAMESPACE") { }
+    public NamespaceProcessor() : base("NAMESPACE") { }
     
     protected override string ProcessExecutor(AssetInfo assetInfo) {
       var targetNamespaceEntities = assetInfo.FilePathEntities
         .SkipLast(1) // remove filename
         .Reverse() // reverse so that going up the tree
-        .TakeWhile(filename => !filename.Equals(AssetsKeyword, StringComparison.OrdinalIgnoreCase)) // take all directories until reach Assets
+        .TakeWhile(filename => !filename.Equals(Constants.AssetsKeyword, StringComparison.OrdinalIgnoreCase)) // take all directories until reach Assets
         .Reverse() // reverse again to regain order
         .Where(filename => !NamespaceExclusions.Contains(filename, StringComparer.CurrentCultureIgnoreCase)); // remove invalid namespace directories
 
---
+++ Processors/YearProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Keyword.Processors {
+  public class YearProcessor : KeywordProcessor {
+    public YearProcessor() : base("YEAR") { }
+    
+    protected override string ProcessExecutor(AssetInfo assetInfo) {
+      return DateTime.Now.ToString("yyyy");
+    }
+  }
+}
