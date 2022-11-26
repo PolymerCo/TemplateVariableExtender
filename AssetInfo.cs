f7d6e62269d7d3b4ed2741cf6222400e05240020
Oliver Mitchell <oliver@polymerco.com.au>
2022-11-26 21:08:48 +1030

    Added structural files for processing and asset info

---
+++ AssetInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace editor.keyword {
+  public sealed class AssetInfo {
+    /** Temp filename associated with keyword processor temp files. */
+    private const string TempExtension = "kwdtmp";
+    
+    public string Name { get; }
+    public string FilePath { get; }
+    public string[] FilePathEntities { get; }
+    public string ParentFilePath { get; }
+    public string FileName { get; }
+    public string FullFileName { get; }
+    public string FileExtension { get; }
+    public string TempFilePath { get; }
+
+    public AssetInfo(string assetName) {
+      Name = assetName;
+      FilePath = Path.GetFullPath(assetName);
+      FilePathEntities = FilePath.Split(Path.DirectorySeparatorChar);
+      ParentFilePath = string.Join(Path.DirectorySeparatorChar, FilePathEntities.SkipLast(1));
+      FileName = Path.GetFileNameWithoutExtension(FilePath);
+      FullFileName = Path.GetFileName(FilePath);
+      FileExtension = Path.GetExtension(FilePath);
+      TempFilePath = $"{ParentFilePath}{Path.DirectorySeparatorChar}.{FullFileName}.{TempExtension}";
+    }
+  }
+}
---
+++ KeywordProcessor.cs
@@ -0,0 +1,20 @@
+﻿
+namespace editor.keyword {
+  public abstract class KeywordProcessor {
+    /** Wrapper for the keywords. */
+    private const char KeywordWrapper = '#';
+
+    /** Keyword to identify replacement targets. */
+    private readonly string _keyword;
+
+    protected KeywordProcessor(string identifier) {
+      _keyword = $"{KeywordWrapper}{identifier}{KeywordWrapper}";
+    }
+    
+    public string Process(AssetInfo assetInfo, string line) {
+      return line.Contains(_keyword) ? line.Replace(_keyword, ProcessExecutor(assetInfo)) : line;
+    }
+
+    protected abstract string ProcessExecutor(AssetInfo assetInfo);
+  }
+}
